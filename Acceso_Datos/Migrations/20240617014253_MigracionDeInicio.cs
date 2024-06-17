e9ebcf65e5e9a50547b02c7be53563b2377a12a3
Nestor.Adonay.Rodriguez.Alberto@gmail.com <nestor.adonay.rodriguez.alberto@gmail.com>
2024-06-16 19:46:38 -0600

    "Creacion De Las Tablas En La DB"

---
+++ Acceso_Datos/Migrations/20240617014253_MigracionDeInicio.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Acceso_Datos.Migrations
+{
+    public partial class MigracionDeInicio : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Empleados",
+                columns: table => new
+                {
+                    IdEmpleado = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Salaraio = table.Column<double>(type: "float", nullable: false),
+                    FechaNacimiento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Fotografia = table.Column<byte[]>(type: "varbinary(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Empleados", x => x.IdEmpleado);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Facturas",
+                columns: table => new
+                {
+                    IdFactura = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FechaRealizada = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    NombreCliente = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Correlativo = table.Column<int>(type: "int", nullable: false),
+                    Total = table.Column<double>(type: "float", nullable: false),
+                    IdEmpleadoEnFactura = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Facturas", x => x.IdFactura);
+                    table.ForeignKey(
+                        name: "FK_Facturas_Empleados_IdEmpleadoEnFactura",
+                        column: x => x.IdEmpleadoEnFactura,
+                        principalTable: "Empleados",
+                        principalColumn: "IdEmpleado",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DetalleFacturas",
+                columns: table => new
+                {
+                    IdDetalleFactura = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NombreProducto = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CantidadComprada = table.Column<int>(type: "int", nullable: false),
+                    PrecioProducto = table.Column<double>(type: "float", nullable: false),
+                    IdFacturaEnDetalleFactura = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DetalleFacturas", x => x.IdDetalleFactura);
+                    table.ForeignKey(
+                        name: "FK_DetalleFacturas_Facturas_IdFacturaEnDetalleFactura",
+                        column: x => x.IdFacturaEnDetalleFactura,
+                        principalTable: "Facturas",
+                        principalColumn: "IdFactura",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DetalleFacturas_IdFacturaEnDetalleFactura",
+                table: "DetalleFacturas",
+                column: "IdFacturaEnDetalleFactura");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Facturas_IdEmpleadoEnFactura",
+                table: "Facturas",
+                column: "IdEmpleadoEnFactura");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DetalleFacturas");
+
+            migrationBuilder.DropTable(
+                name: "Facturas");
+
+            migrationBuilder.DropTable(
+                name: "Empleados");
+        }
+    }
+}
